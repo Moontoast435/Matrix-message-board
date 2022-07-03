8671a435e9a89e9c972855a81e1d8a4491d6fbb0
Your Name <seanmalone1993@gmail.com>
2022-07-03 01:12:18 +0100

    Added Giphy API to index.js

--- Client/Assets/index.js
+++ Client/Assets/index.js
@@ -1,20 +1,21 @@
 // Initialising the canvas
-var canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d');
+var canvas = document.querySelector("canvas"),
+  ctx = canvas.getContext("2d");
 
 // Setting the width and height of the canvas
-canvas.width = window.innerWidth; 
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 console.log(canvas.width, canvas.height);
 
 // Setting up the letters
-var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
-letters = letters.split('');
+var letters =
+  "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ";
+letters = letters.split("");
 
 // Setting up the columns
 var fontSize = 10,
-    columns = canvas.width / fontSize;
+  columns = canvas.width / fontSize;
 
 // Setting up the drops
 var drops = [];
@@ -24,14 +25,14 @@ for (var i = 0; i < columns; i++) {
 
 // Setting up the draw function
 function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.fillStyle = "rgba(0, 0, 0, .1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (var i = 0; i < drops.length; i++) {
     var text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#0f0';
+    ctx.fillStyle = "#0f0";
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
       drops[i] = 0;
     }
   }
@@ -40,3 +41,31 @@ function draw() {
 // Loop the animation
 setInterval(draw, 33);
 
+// Giphy API key
+let APIKEY = "bsmGT5Kv6ZHaU7EQ6wHi6rbj174B65M2";
+
+// linking Giphy API to DOM
+
+/* document.addEventListener("DOMContentLoaded", init);
+function init() {
+  document
+    .getElementById("gif-search-button") //placeholder ID used, change to whatever the gif search button is
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=10&g=`;
+      let str = document.getElementById("search").value.trim();
+      url = url.concat(str); // adds search query onto the url before fetching it
+      fetch(url)
+        .then((res) => res.json())
+        .then((content) => {
+          console.log(content.data); //contains all the GIF data
+          console.log("META", content.meta); // the meta just tells us if the query was successful or not
+          let gifSelection = document.createElement("div");
+          content.data.forEach((gif) => {
+            let img = document.createElement("img");
+            img.src = gif.images.downsized.url;
+            gifSelection.appendChild(imgSrc); // This **should** create a menu which displays all of the GIFs you searched for
+          });
+        });
+    });
+} */

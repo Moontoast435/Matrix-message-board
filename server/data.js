73db916d96d0a94b034a9838f35c16ae0e05cc04
sam <sampurna.chapagainnn@gmail.com>
2022-07-03 13:22:53 +0100

    update data.js file with new field,react object with like,dislike and heart fields

--- server/data.js
+++ server/data.js
@@ -1,11 +1,32 @@
 const messages = [
   {
+    id: 1,
     message: "example message",
     comments: [
       { id: 1, comment: "example comment" },
       { id: 2, comment: "example comment" },
       { id: 3, comment: "example comment" },
     ],
+    react: {
+      like: 1,
+      dislike: 0,
+      heart: 0
+    }
+  },
+
+  {
+    id: 2,
+    message: "new example message",
+    comments: [
+      { id: 1, comment: "new example comment" },
+      { id: 2, comment: "new example comment" },
+      { id: 3, comment: "new example comment" },
+    ],
+    react: {
+      like: 0,
+      dislike: 1,
+      heart: 0
+    }
   },
 ];
 

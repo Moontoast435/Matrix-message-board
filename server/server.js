220cdeba67f1b5712d49c3c1bf630f00bb7b088a
Your Name <seanmalone1993@gmail.com>
2022-07-05 13:49:01 +0100

    changed port variable on server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () =>
-  console.log(`Server online at http://localhost:${port}`)
+app.listen(PORT, () =>
+  console.log(`Server online at http://localhost:${PORT}`)
 );

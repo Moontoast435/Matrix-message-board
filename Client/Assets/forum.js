a7115d1a1883b1e4b4820f9a2259a8748c765289
codingkompot <ksqasiyah@protonmail.com>
2022-07-06 00:45:52 +0100

    fixed delete function

--- Client/Assets/forum.js
+++ Client/Assets/forum.js
@@ -21,17 +21,6 @@ function postMessage(e) {
   });
   window.location.reload(); //reloads the page so that the new message shows up
 }
-
-async function deletePost(url) {
-  await fetch(url, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  window.location.reload();
-}
-
 let postContainer = document.getElementById("post-container");
 
 // This functions will display all of the posts from the API to the forum page
@@ -102,12 +91,25 @@ function getMessages() {
         deleteBtn.classList = "delete_button";
         deleteBtn.textContent = "DELETE";
         deleteBtn.id = "delete_button" + msgId;
-        deleteBtn.addEventListener("click", deletePost(API_URL + "/" + msgId));
-
+        deleteBtn.addEventListener("click", () => {
+          deletePost(API_URL + "/" + msgId)
+        
+        });
         post.appendChild(deleteBtn);
       })
     );
 }
+
+async function deletePost(url) {
+  const resp = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+  window.location.reload();
+}
+
 
 /*function appendResults(data) {
   data.forEach((r) => {

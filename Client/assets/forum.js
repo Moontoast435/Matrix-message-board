76fd63ec0e99f89343d7b709b326a0fafb8996d2
codingkompot <ksqasiyah@protonmail.com>
2022-07-06 15:34:14 +0100

    added modal to GIF button

--- Client/assets/forum.js
+++ Client/assets/forum.js
@@ -111,6 +111,22 @@ async function deletePost(url) {
   }
 }
 
+// Show GIFs
+
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+
+
 /*function appendResults(data) {
   data.forEach((r) => {
     postResults.append(makeMessage(r.message));

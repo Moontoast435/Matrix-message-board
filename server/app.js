c7c1c734c6f9b5bea9f391fa2ced047945f1da41
Your Name <seanmalone1993@gmail.com>
2022-07-07 11:28:12 +0100

    moved path package

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const messages = require("./data");
-
+const path = require("path");
 const msgRoutes = require("./controllers/routes");
 
 if (process.env.NODE_ENV === "production") {

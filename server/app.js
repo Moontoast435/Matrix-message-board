ff0a5516c71618d935b62cd69b1bd8185dd1667d
Your Name <seanmalone1993@gmail.com>
2022-07-07 11:15:06 +0100

    more changes to app.js and server.js

--- server/app.js
+++ server/app.js
@@ -10,12 +10,4 @@ app.use(cors("*"));
 app.use("/messages", msgRoutes);
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 module.exports = app;
